06443e17338fd45bb7b8212fb5c4bf80082e3520
Darya Tarasova <kuroifreya@gmail.com>
2023-02-07 20:45:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketCost = 20.1;
+        int miles;
+
+        miles=(int) (ticketCost / 20);
+
+        System.out.println("За этот билет вы получите миль: " + miles);
+    }
+}
